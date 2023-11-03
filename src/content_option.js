d949be656c20b61ef999ba246c14b81271b51946
Karl Gregor Rauniste <karl-gregor@veebisepad.ee>
2023-11-03 13:22:08 +0200

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,182 @@
+const logotext = "K / G / R";
+const meta = {
+  title: "Karl Gregor Rauniste",
+  description:
+    "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Karl Gregor Rauniste",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I mentor next generation",
+  },
+  description:
+    "Sometimes maybe good, sometimes maybe shit",
+  your_img_url: "/images/eebissupid_6.jpg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "My job responsibilities include close communication with clients to understand their needs and desires, and then developing suitable software solutions. I am responsible for planning, managing, and executing projects, ensuring that they are accurate, stable, and secure.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Front-end Developer",
+    where: "Veebisepad OÜ",
+    date: "2020",
+  },
+  {
+    jobtitle: "IT-Teacher",
+    where: "Kuressaare Ametikool",
+    date: "2021",
+  },
+  {
+    jobtitle: "Project Maranger",
+    where: "Flowershower OÜ",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Tailwind",
+    value: 85,
+  },
+  {
+    name: "HTML & CSS",
+    value: 90,
+  },
+  {
+    name: "VueJs",
+    value: 50,
+  },
+  {
+    name: "Javascript",
+    value: 50,
+  },
+  {
+    name: "Laravel",
+    value: 40,
+  },
+  {
+    name: "Negotiation",
+    value: 90,
+  },
+  {
+    name: "Time managment",
+    value: 75,
+  },
+  {
+    name: "Collaboration",
+    value: 80,
+  },
+  {
+    name: "Critical thinking",
+    value: 70,
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Eraspordikool Sepps",
+    link: "https://sepps.eu/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Tourmotor",
+    link: "https://tourmotor.net/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Flowershower",
+    link: "https://flowershower.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "Waag",
+    link: "https://waag.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "KTG",
+    link: "https://www.ktg.edu.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Kalma Ehitus",
+    link: "https://kalmaehitus.ee/",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "Babycool food",
+    link: "https://babycoolfood.com/",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "Namesong",
+    link: "https://namesong.com/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Navicup",
+    link: "https://navicup.com/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "BMI Monier Roof calculator(EST)",
+    link: "https://calcee.bmieesti.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "BMI Monier Roof calculator(LV)",
+    link: "https://calclv.bmieesti.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Lapitekk",
+    link: "https://www.lapitekk.ee/",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Alar Truu Fotograph",
+    link: "https://alartruu.com/",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "Valmar Voolaid Fotograph",
+    link: "https://valmarifoto.com/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "karl-gregor@veebisepad.ee",
+  YOUR_FONE: "(+372)5270-692",
+//   description:
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Swissgroover",
+  linkedin: "https://www.linkedin.com/in/karl-gregor-rauniste/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
+
+export const Portfolio = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Portfolio | {meta.title} </title>{" "}
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <p className="pb-4">
+              The pages and client projects within the portfolio are crafted
+              with the Vue.js, Laravel, and Statamic frameworks, while the page
+              designs were the creative work of another individual.
+            </p>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {dataportfolio.map((data, i) => {
+            return (
+              <div key={i} className="po_item">
+                <img src={data.img} alt="" />
+                <div className="content">
+                  <p className="pb-4">{data.description}</p>
+                  <a href={data.link} target="_blank" rel="noreferrer">
+                    view project
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </HelmetProvider>
+  );
+};
